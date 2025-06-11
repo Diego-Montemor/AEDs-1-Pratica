2a9310432543cb61c51e193e070e8a66752e2007
Diego Montemor <montemordiego@gmail.com>
2025-06-11 18:14:30 -0300

    Adiciona comentários explicativos ao código para melhor compreensão

--- ExercicioVetor.cpp
+++ ExercicioVetor.cpp
@@ -11,17 +11,18 @@ int main(int argc, char** argv) {
     QuantAtual = quant - NExcluido;
     srand(time(NULL));
 
+    //gera uma tabela com a quantidade de elementos aleatorios desejada
     cout << "A tabela gerada foi: ";
     for (int i = 0; i < quant; i++) {
         tabela[i] = rand() % (30 - 1 + 1);
         cout << tabela[i] << " ";
     }
 
-    while (opcao != 9) {
+    while (opcao != 9) {// mantem o loop até que a opção 9 seja escolhida
         cout << "\nQual opção você deseja?\n(1).inverter os valores\n(2).adicionar um valor a todos\n(3).adicionar um valor a todos numeros não divisiveis por 3\n(4).excluir um valor\n(5).excluir todos elementos de um valor expecifico\n(6).adicionar um valor\n(7).adicionar um valor a um lugar expecifico\n(8).Excluir todos valores repetidos\n(9).Fim\nR: ";
         cin >> opcao;
-        switch (opcao) {
-            case 1: // Parte 1
+        switch (opcao) { // separa cada opção em um case
+            case 1: // Parte 1 flipa os valores de tras para frente
                 for (int i = 0; i < QuantAtual / 2; i++) {
                     int n1, n2;
                     n1 = tabela[i];
@@ -35,7 +36,7 @@ int main(int argc, char** argv) {
                     cout << tabela[i] << " ";
                 }
                 break;
-            case 2: // Parte 2
+            case 2: // Parte 2 soma um valor a todos os elementos da tabela
                 int valor;
 
                 cout << "\nDigite um valor você deseja adicionar a todos os valores: ";
@@ -47,7 +48,7 @@ int main(int argc, char** argv) {
                     cout << tabela[i] << " ";
                 }
                 break;
-            case 3: // Parte 3
+            case 3: // Parte 3 soma um valor a todos os elementos que não são divisiveis por 3
                 cout << "\nA tabela gerada pela parte 3 foi: ";
                 for (int i = 0; i < QuantAtual; i++) {
                     if (not tabela[i] % 3 == 0) {
@@ -56,7 +57,7 @@ int main(int argc, char** argv) {
                     cout << tabela[i] << " ";
                 }
                 break;
-            case 4: // Parte 4
+            case 4: // Parte 4 exclui a primeira ocorrência de um valor
                 int excluir, z;
                 z = 0;
                 cout << "Digite o valor que você deseja excluir: ";
@@ -83,7 +84,7 @@ int main(int argc, char** argv) {
                     cout << tabela[i] << " ";
                 }
                 break;
-            case 5: // Parte 5
+            case 5: // Parte 5 exclui todas as ocorrências de um valor
                 int ex;
                 cout << "Digite o valor que você deseja excluir: ";
                 cin >> ex;
@@ -108,7 +109,7 @@ int main(int argc, char** argv) {
                     cout << tabela[i] << " ";
                 }
                 break;
-            case 6: // Parte 6
+            case 6: // Parte 6 adiciona um valor ao final da tabela
                 if (QuantAtual < quant) {
                     int add;
                     cout << "Digite qual valor você deseja adicionar: ";
@@ -122,7 +123,7 @@ int main(int argc, char** argv) {
                     }
                 } else cout << "A tabela ja está cheia";
                 break;
-            case 7: // Parte 7
+            case 7: // Parte 7 adiciona um valor em uma posição específica
                 if (QuantAtual < quant) {
                     int add, pos;
                     cout << "Digite qual valor você deseja adicionar: ";
@@ -142,7 +143,7 @@ int main(int argc, char** argv) {
                     }
                 } else cout << "A tabela ja está cheia";
                 break;
-            case 8: // Parte 8
+            case 8: // Parte 8 exclui todos os valores repetidos
                 for (int i = 0; i < QuantAtual; i++) {
                     int n1 = tabela[i], igual = 0;
                     for (int j = 0; j < QuantAtual; j++) {
@@ -175,7 +176,7 @@ int main(int argc, char** argv) {
                     cout << tabela[i] << " ";
                 }
                 break;
-            case 9: // Parte 9
+            case 9: // Parte 9 encerra o programa
                 opcao = 9;
                 break;
         }
