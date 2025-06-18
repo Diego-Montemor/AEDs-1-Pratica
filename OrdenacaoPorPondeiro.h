bc61b8aabcbf115128f0ef1098d7871fb82fbfc2
Diego Montemor <montemordiego@gmail.com>
2025-06-18 14:57:12 -0300

    Adiciona implementações das funções de ordenação Bubble Sort, Selection Sort e Insertion Sort com uso de ponteiros

---
+++ OrdenacaoPorPondeiro.h
@@ -0,0 +1,9 @@
+#ifndef ORDENACAOPORPONTEIRO_H
+#define ORDENACAOPORPONTEIRO_H
+
+void bubblePonteiro(int Vetor[], int tam);
+void selectionPonteiro(int Vetor[], int tam);
+void insertionPonteiro(int Vetor[], int tam);
+
+
+#endif
